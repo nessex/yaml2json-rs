3c4cd190b5af9b4886707c53f27a49fb607045ec
Nathan Essex <nathan@essex.id.au>
2020-11-16 22:52:35 +0900

    Add docs for yaml2json-rs

--- crates/yaml2json-rs/src/lib.rs
+++ crates/yaml2json-rs/src/lib.rs
@@ -15,20 +15,103 @@ pub enum Yaml2JsonError {
     IOError(#[from] std::io::Error),
 }
 
+/// `Style`s define JSON output formats for `Yaml2Json`.
 pub enum Style {
+    /// `Style::COMPACT` outputs JSON on a single line.
+    /// e.g. for the following YAML:
+    /// ```yaml
+    /// ---
+    /// hello: world
+    /// spec:
+    ///   items:
+    ///     - a
+    ///     - b
+    /// ```
+    ///
+    /// the JSON output will be:
+    /// ```json
+    /// {"hello":"world","spec":{"items":["a","b"]}}
+    /// ```
     COMPACT,
+    /// `Style::PRETTY` outputs JSON on multiple lines, with automatic indentation.
+    /// e.g. for the following YAML:
+    /// ```yaml
+    /// ---
+    /// hello: world
+    /// spec:
+    ///   items:
+    ///     - a
+    ///     - b
+    /// ```
+    ///
+    /// the JSON output will be:
+    /// ```json
+    /// {
+    ///   "hello": "world",
+    ///   "spec": {
+    ///     "items": [
+    ///       "a",
+    ///       "b"
+    ///     ]
+    ///   }
+    /// }
+    /// ```
     PRETTY,
 }
 
+/// Yaml2Json can convert individual YAML documents into JSON. Each instance can be configured to
+/// have different styles of output.
+///
+/// The JSON output can be returned as a string:
+/// ```
+/// use yaml2json_rs::{Yaml2Json, Style};
+///
+/// let y2j = Yaml2Json::new(Style::COMPACT);
+/// let input = String::from("hello: world");
+/// let output = y2j.document_to_string(input).unwrap();
+///
+/// assert_eq!(output, r#"{"hello":"world"}"#);
+/// ```
+///
+/// Or, the JSON output can be sent to a writer:
+/// ```
+/// use yaml2json_rs::{Yaml2Json, Style};
+/// use std::io;
+///
+/// let y2j = Yaml2Json::new(Style::COMPACT);
+/// let input = String::from("hello: world");
+/// let mut stdout = io::stdout();
+///
+/// y2j.document_to_writer(input, &mut stdout);
+///
+/// // {"hello":"world"}
+/// ```
 pub struct Yaml2Json {
     style: Style,
 }
 
 impl Yaml2Json {
+    /// `new()` creates a new `Yaml2Json`. It expects you to provide an output `Style`.
+    /// ```
+    /// use yaml2json_rs::{Yaml2Json, Style};
+    ///
+    /// let y2j_pretty = Yaml2Json::new(Style::PRETTY);
+    /// let y2j_compact = Yaml2Json::new(Style::COMPACT);
+    /// ```
     pub fn new(style: Style) -> Self {
         Self { style }
     }
 
+    /// `document_to_string()` takes a YAML document String and converts it to a JSON String.
+    /// ```
+    /// use yaml2json_rs::{Yaml2Json, Style};
+    ///
+    /// let y2j = Yaml2Json::new(Style::COMPACT);
+    /// let input = String::from("hello: world");
+    /// let output = y2j.document_to_string(input).unwrap();
+    ///
+    /// assert_eq!(output, r#"{"hello":"world"}"#);
+    /// ```
     pub fn document_to_string(&self, document: String) -> Result<String, Yaml2JsonError> {
         let s: serde_json::Value = serde_yaml::from_str(document.as_str())?;
 
@@ -43,6 +126,21 @@ impl Yaml2Json {
         }
     }
 
+    /// `document_to_writer()` takes a YAML document string, converts it to JSON and sends the output
+    /// to the provided writer.
+    ///
+    /// ```
+    /// use yaml2json_rs::{Yaml2Json, Style};
+    /// use std::io;
+    ///
+    /// let y2j = Yaml2Json::new(Style::COMPACT);
+    /// let input = String::from("hello: world");
+    /// let mut stdout = io::stdout();
+    ///
+    /// y2j.document_to_writer(input, &mut stdout);
+    ///
+    /// // {"hello":"world"}
+    /// ```
     pub fn document_to_writer<W: io::Write>(
         &self,
         document: String,
